d39f86af143b7194e80799ce60f36765a758dd26
Kingsly Mokgwathi <s220191506@mandela.ac.za>
2024-01-30 10:37:56 +0200

    first commit

---
+++ src/main/java/com/kingsly/robotapocalypse/RobotapocalypseApplication.java
@@ -0,0 +1,13 @@
+package com.kingsly.robotapocalypse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotapocalypseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotapocalypseApplication.class, args);
+	}
+
+}
